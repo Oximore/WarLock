a1ae3bbad5694e4ae5ccd71e08042a853f8b242c
Benjamin <benjamin@Nebulja.home>
2013-02-19 17:08:23 +0100

    ajout d'un main

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+
+all: a.out
+	g++ coord.cpp entity.cpp grid.cpp main.cpp player.cpp
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include "grid.h"
+#include "entity.h"
+using namespace std ;
+
+
+int main()
+{
+  Grid * grid = new Grid(5,5,2);
+  
+  
+  
+  
+  bool gameOver = false;
+
+  int deplacementJoueur1 = 0;
+  int deplacementJoueur2 = 0;
+  
+  while(gameOver) {
+    
+    grid->print();
+    cout << "Déplacement (de 1 à 9): ..." << endl;
+    cin >> deplacementJoueur1;
+    
+    deplacementJoueur2 = 1 + rand()%9;
+    
+    grid->resDep(deplacementJoueur1, deplacementJoueur2);
+    cout << endl;
+    
+  }
+  return 0;
+};
